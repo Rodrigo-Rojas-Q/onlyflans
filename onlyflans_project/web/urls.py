9112f4eea2939ad6e01bd6d562f0fb913f443b6c
Rodrigo-Rojas-Q <thewalkpretender@gmail.com>
2024-08-01 14:28:13 -0400

    hito n3

--- onlyflans_project/web/urls.py
+++ onlyflans_project/web/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('welcome/', views.welcome, name='welcome'),
     path('base/', views.base, name='base'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('exito/', views.exito, name='exito'),
 ]
--- onlyflans_project/web/views.py
+++ onlyflans_project/web/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Flan
+from . forms import ContactFormForm
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
-    return render(request, 'index.html')
+    flanes_publicos = Flan.objects.filter(is_private=False)
+    return render(request, 'index.html',{'flanes': flanes_publicos})
 
 def about(request):
     return render(request, 'about.html')
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    flanes_privados = Flan.objects.filter(is_private=True)
+    return render(request, 'welcome.html',{'flanes': flanes_privados})
 
 def base(request):
     return render(request, 'base.html')
+
+def contacto(request):
+    if request.method == 'POST':
+        form = ContactFormForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/exito/')
+    else:
+        form = ContactFormForm()
+    return render(request, 'contacto.html', {'form':form})
+
+def exito(request):
+    return render(request, 'exito.html')
+    
+    
