725c566cc1759ac65fce1880f0825ce92845ff5f
Rodrigo-Rojas-Q <thewalkpretender@gmail.com>
2024-08-01 21:31:57 -0400

    hito n3.2

--- onlyflans_project/web/forms.py
+++ onlyflans_project/web/forms.py
@@ -2,21 +2,12 @@ from django import forms
 from .models import ContactForm
 
 class ContactFormForm(forms.ModelForm):
+    Correo_Electronico = forms.EmailField(label='Correo')
+    Nombre = forms.CharField(max_length=64, label='Nombre')
+    Mensaje = forms.CharField(
+        label='Mensaje',
+        widget=forms.Textarea(attrs={'rows': 5, 'cols': 40, 'class': 'form-control'})
+    )
     class Meta:
-        model = ContactForm
-        fields = ['Correo_Electronico', 'Nombre', 'Mensaje']
-        widgets = {
-            'Correo_Electronico': forms.EmailInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Tu correo electrónico'
-            }),
-            'Nombre': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Tu nombre'
-            }),
-            'Mensaje': forms.Textarea(attrs={
-                'class': 'form-control',
-                'placeholder': 'Escribe tu mensaje aquí',
-                'rows': 5
-            }),
-        }
+         model = ContactForm
+         fields = ['Correo_Electronico', 'Nombre', 'Mensaje']
